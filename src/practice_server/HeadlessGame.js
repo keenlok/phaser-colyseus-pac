79b63516af2d6bc6f1c8286800a8ce9524d7d313
Keen Lok <keenloklai@gmail.com>
2019-05-31 12:20:16 +0800

    Update file paths

--- src/practice_server/HeadlessGame.js
+++ src/practice_server/HeadlessGame.js
@@ -20,10 +20,10 @@ class HeadlessGame extends Phaser.Scene {
 
   loadAssets () {
     HeadlessGame.messageLog("Preload: Backgrounds")
-    this.load.image('background', './public/assets/background-2x.png')
+    this.load.image('background', '../../public/assets/background-2x.png')
 
     HeadlessGame.messageLog("Preload: Dots")
-    this.load.spritesheet('specialDot', './public/assets/dots.png', {
+    this.load.spritesheet('specialDot', '../../public/assets/dots.png', {
       frameHeight: 16,
       frameWidth: 16
     })
@@ -37,39 +37,39 @@ class HeadlessGame extends Phaser.Scene {
   }
 
   loadMap () {
-    this.load.image('tile', './public/assets/map/final-extruded-again.png')
-    this.load.image('foodTile', './public/assets/map/food.png')
-    this.load.spritesheet('powerup', './public/assets/map/power-up-normal.png', {
+    this.load.image('tile', '../../public/assets/map/final-extruded-again.png')
+    this.load.image('foodTile', '../../public/assets/map/food.png')
+    this.load.spritesheet('powerup', '../../public/assets/map/power-up-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.tilemapTiledJSON('newmap', './public/assets/map/map.json')
+    this.load.tilemapTiledJSON('newmap', '../../public/assets/map/map.json')
 
-    this.load.image('coral', './public/assets/map/coral-small-v1.png')
+    this.load.image('coral', '../../public/assets/map/coral-small-v1.png')
   }
 
   loadSprites () {
     HeadlessGame.messageLog("Preload: Sprites: Scuttles")
 
     /** Scuttles spritesheets */
-    this.load.spritesheet('scuttle', './public/assets/sprites/player/original/scuttle.png', {
+    this.load.spritesheet('scuttle', '../../public/assets/sprites/player/original/scuttle.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('scuttle_die', './public/assets/sprites/player/original/crab-death.png', {
+    this.load.spritesheet('scuttle_die', '../../public/assets/sprites/player/original/crab-death.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('scuttle_spawn', './public/assets/sprites/player/original/crab-pop.png', {
+    this.load.spritesheet('scuttle_spawn', '../../public/assets/sprites/player/original/crab-pop.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('scuttle_wobble', './public/assets/sprites/player/original/crab-wobble.png', {
+    this.load.spritesheet('scuttle_wobble', '../../public/assets/sprites/player/original/crab-wobble.png', {
       frameWidth: 160,
       frameHeight: 160
     })
 
-    this.load.spritesheet('happy', './public/assets/sprites/player/original/superhappy.png', {
+    this.load.spritesheet('happy', '../../public/assets/sprites/player/original/superhappy.png', {
       frameWidth: 160,
       frameHeight: 160
     })
@@ -81,15 +81,15 @@ class HeadlessGame extends Phaser.Scene {
   loadEnemies () {
     HeadlessGame.messageLog("Preload: Sprites: Enemies: Commons")
 
-    this.load.spritesheet('enemy_spawn', './public/assets/sprites/enemies/enemy-pop.png', {
+    this.load.spritesheet('enemy_spawn', '../../public/assets/sprites/enemies/enemy-pop.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('enemy_wobble', './public/assets/sprites/enemies/enemy-wobble.png', {
+    this.load.spritesheet('enemy_wobble', '../../public/assets/sprites/enemies/enemy-wobble.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('dead_spirit', './public/assets/sprites/enemies/glow-loop.png', {
+    this.load.spritesheet('dead_spirit', '../../public/assets/sprites/enemies/glow-loop.png', {
       frameWidth: 160,
       frameHeight: 160
     })
@@ -102,23 +102,23 @@ class HeadlessGame extends Phaser.Scene {
   loadHermit () {
     HeadlessGame.messageLog("Preload: Sprites: Enemies: Hermit")
 
-    this.load.spritesheet('hermit_left', './public/assets/sprites/enemies/hermit/hermit-left-normal.png', {
+    this.load.spritesheet('hermit_left', '../../public/assets/sprites/enemies/hermit/hermit-left-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('hermit_hunt_left', './public/assets/sprites/enemies/hermit/hermit-left-hunt.png', {
+    this.load.spritesheet('hermit_hunt_left', '../../public/assets/sprites/enemies/hermit/hermit-left-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('hermit_right', './public/assets/sprites/enemies/hermit/hermit-right-normal.png', {
+    this.load.spritesheet('hermit_right', '../../public/assets/sprites/enemies/hermit/hermit-right-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('hermit_hunt_right', './public/assets/sprites/enemies/hermit/hermit-right-hunt.png', {
+    this.load.spritesheet('hermit_hunt_right', '../../public/assets/sprites/enemies/hermit/hermit-right-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('hermit_dying', './public/assets/sprites/enemies/hermit/hermit-death.png', {
+    this.load.spritesheet('hermit_dying', '../../public/assets/sprites/enemies/hermit/hermit-death.png', {
       frameWidth: 160,
       frameHeight: 160
     })
@@ -128,23 +128,23 @@ class HeadlessGame extends Phaser.Scene {
     HeadlessGame.messageLog("Preload: Sprites: Enemies: Jelly")
 
     /** Jelly */
-    this.load.spritesheet('jelly_left', './public/assets/sprites/enemies/jelly/jelly-left-normal.png', {
+    this.load.spritesheet('jelly_left', '../../public/assets/sprites/enemies/jelly/jelly-left-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('jelly_right', './public/assets/sprites/enemies/jelly/jelly-right-normal.png', {
+    this.load.spritesheet('jelly_right', '../../public/assets/sprites/enemies/jelly/jelly-right-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('jelly_hunt_left', './public/assets/sprites/enemies/jelly/jelly-left-hunt.png', {
+    this.load.spritesheet('jelly_hunt_left', '../../public/assets/sprites/enemies/jelly/jelly-left-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('jelly_hunt_right', './public/assets/sprites/enemies/jelly/jelly-right-hunt.png', {
+    this.load.spritesheet('jelly_hunt_right', '../../public/assets/sprites/enemies/jelly/jelly-right-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('jelly_dying', './public/assets/sprites/enemies/jelly/jelly-death.png', {
+    this.load.spritesheet('jelly_dying', '../../public/assets/sprites/enemies/jelly/jelly-death.png', {
       frameWidth: 160,
       frameHeight: 160
     })
@@ -154,19 +154,19 @@ class HeadlessGame extends Phaser.Scene {
     HeadlessGame.messageLog("Preload: Sprites: Enemies: Shark")
 
     /** shark */
-    this.load.spritesheet('shark_left', './public/assets/sprites/enemies/shark/shark-left-normal.png', {
+    this.load.spritesheet('shark_left', '../../public/assets/sprites/enemies/shark/shark-left-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('shark_right', './public/assets/sprites/enemies/shark/shark-right-normal.png', {
+    this.load.spritesheet('shark_right', '../../public/assets/sprites/enemies/shark/shark-right-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('shark_hunt_left', './public/assets/sprites/enemies/shark/shark-left-hunt.png', {
+    this.load.spritesheet('shark_hunt_left', '../../public/assets/sprites/enemies/shark/shark-left-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('shark_hunt_right', './public/assets/sprites/enemies/shark/shark-right-hunt.png', {
+    this.load.spritesheet('shark_hunt_right', '../../public/assets/sprites/enemies/shark/shark-right-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
@@ -176,23 +176,23 @@ class HeadlessGame extends Phaser.Scene {
     HeadlessGame.messageLog("Preload: Sprites: Enemies: Octopus")
 
     /** octo */
-    this.load.spritesheet('octo_left', './public/assets/sprites/enemies/octo/octo-left-normal.png', {
+    this.load.spritesheet('octo_left', '../../public/assets/sprites/enemies/octo/octo-left-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('octo_right', './public/assets/sprites/enemies/octo/octo-right-normal.png', {
+    this.load.spritesheet('octo_right', '../../public/assets/sprites/enemies/octo/octo-right-normal.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('octo_hunt_left', './public/assets/sprites/enemies/octo/octo-left-hunt.png', {
+    this.load.spritesheet('octo_hunt_left', '../../public/assets/sprites/enemies/octo/octo-left-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('octo_hunt_right', './public/assets/sprites/enemies/octo/octo-right-hunt.png', {
+    this.load.spritesheet('octo_hunt_right', '../../public/assets/sprites/enemies/octo/octo-right-hunt.png', {
       frameWidth: 160,
       frameHeight: 160
     })
-    this.load.spritesheet('octo_dying', './public/assets/sprites/enemies/octo/octo-death.png', {
+    this.load.spritesheet('octo_dying', '../../public/assets/sprites/enemies/octo/octo-death.png', {
       frameWidth: 160,
       frameHeight: 160
     })
